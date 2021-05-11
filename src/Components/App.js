e67d9ef55e01cda64e638be05ded08b3e3da13b0
Godswilldev <gimmex1@gmail.com>
2021-05-11 14:21:08 +0100

    Add project Starter Files

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// import background from "../images/bg-desktop.svg";
+class App extends Component {
+  render() {
+    return (
+      <div className="greg">
+        <div className="hero__background">greg</div>
+      </div>
+    );
+  }
+}
+export default App;
